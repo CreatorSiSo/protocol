9ddded5f008dd7e268ea436e885ee24644b5a2d3
Nika Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2025-01-08 18:14:29 +0100

    Add custom panic handler for arduino.

--- adapter-nano/src/main.rs
+++ adapter-nano/src/main.rs
@@ -1,8 +1,40 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use common;
-use panic_halt as _;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    // disable interrupts - firmware has panicked so no ISRs should continue running
+    avr_device::interrupt::disable();
+
+    // SAFETY: Because main() already has references to the peripherals this is an unsafe
+    // operation - but because no other code can run after the panic handler was called,
+    // we know it is okay.
+    let dp = unsafe { arduino_hal::Peripherals::steal() };
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    // Print out panic location
+    ufmt::uwriteln!(&mut serial, "Firmware panic!\r").unwrap_infallible();
+    if let Some(loc) = info.location() {
+        ufmt::uwriteln!(
+            &mut serial,
+            "  At {}:{}:{}\r",
+            loc.file(),
+            loc.line(),
+            loc.column(),
+        )
+        .unwrap_infallible();
+    }
+
+    let mut led = pins.d13.into_output();
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+    }
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -12,7 +44,7 @@ fn main() -> ! {
     let mut led = pins.d13.into_output();
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(100);
+        arduino_hal::delay_ms(1000);
+        unreachable!();
     }
 }
